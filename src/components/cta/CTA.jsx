f16d121d778c64221ae51ce703b48120c53ebaf1
Adrian <aolguin8097@gmail.com>
2022-01-25 10:57:33 -0800

    Imported remaining template

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./cta.css";
 
-const CTA = () => {
-  return <div>CTA</div>;
-};
+const CTA = () => (
+  <div className="ao__cta">
+    <div className="ao__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
+    </div>
+    <div className="ao__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
+);
 
 export default CTA;
